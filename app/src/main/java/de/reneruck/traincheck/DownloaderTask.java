f0ffff5825886c821a70956bf0f0f1225aaa161f
Rene Ruck <rene.ruck@eaglealpha.com>
2015-11-16 23:29:03 +0000

    messing with the downloader async task

--- app/src/main/java/de/reneruck/traincheck/DownloaderTask.java
+++ app/src/main/java/de/reneruck/traincheck/DownloaderTask.java
@@ -1,20 +1,19 @@
 package de.reneruck.traincheck;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.os.AsyncTask;
-import android.util.Log;
-import android.util.Xml;
 
-import org.xmlpull.v1.XmlPullParser;
-import org.xmlpull.v1.XmlPullParserException;
-
-import java.io.IOException;
 import java.io.InputStream;
-import java.lang.reflect.Array;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.ArrayList;
 import java.util.List;
+
+import de.reneruck.traincheck.connection.ApiRequest;
+import de.reneruck.traincheck.connection.ApiResponseParser;
+import de.reneruck.traincheck.models.StationData;
+import de.reneruck.traincheck.receivers.UpdateTrackingReceiver;
 
 /**
  * Created by reneruck on 13/11/2015.
@@ -23,76 +22,60 @@ import java.util.List;
  */
 public class DownloaderTask extends AsyncTask<String, Void, List<StationData>> {
     private static final String DEBUG_TAG = "DownalodTask";
+    private final NotificationManager mNotificationManager;
 
-    private Context activityContext;
+    private Context mContext;
 
-    public DownloaderTask(Context activityContext) {
-        this.activityContext = activityContext;
+    private String[] stations = {"LDWNE", "ASHTN", ""};
+
+    public DownloaderTask(Context context) {
+        mContext = context;
+        mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
     }
 
     @Override
     protected List<StationData> doInBackground(String... endpoints) {
+        String station = "ASHTN";
+        InputStream is = new ApiRequest("http://api.irishrail.ie/realtime/realtime.asmx/getStationDataByCodeXML?StationCode=" + station).execute();
 
-        // params comes from the execute() call: params[0] is the url.
-        try {
-            return downloadUrl(endpoints[0]);
-        } catch (IOException e) {
-            return new ArrayList<StationData>();
-        }
+        List<StationData> stationDataList = ApiResponseParser.parseResult(is);
+        return pickTrackedOnes(stationDataList);
     }
 
-    private List<StationData> downloadUrl(String endpoint) throws IOException {
-        InputStream is = null;
-
-        try {
-            URL url = new URL(endpoint);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setReadTimeout(10000 /* milliseconds */);
-            conn.setConnectTimeout(15000 /* milliseconds */);
-            conn.setRequestMethod("GET");
-            conn.setDoInput(true);
-            // Starts the query
-            conn.connect();
-            int response = conn.getResponseCode();
-            Log.d(DEBUG_TAG, "The response is: " + response);
-            is = conn.getInputStream();
-
-            List<StationData> stationData = parseResult(is);
-            return stationData;
-
-        } finally {
-            if (is != null) {
-                is.close();
-            }
-        }
+    private List<StationData> pickTrackedOnes(List<StationData> stationDataList) {
+        return stationDataList.subList(0, 1);
     }
 
-    private List<StationData> parseResult(InputStream is) throws IOException {
-        List<StationData> results = new ArrayList<StationData>();
-        try {
-            XmlPullParser parser = Xml.newPullParser();
-            parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
-            parser.setInput(is, null);
-            parser.nextTag();
-            return results ; //readFeed(parser);
-        } catch (XmlPullParserException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            is.close();
-        }
-        return results;
-    }
-
-    // onPostExecute displays the results of the AsyncTask.
     @Override
-    protected void onPostExecute(List results) {
-        if(results.isEmpty()){
-//            "Unable to retrieve web page. URL may be invalid.";
-        } else {
+    protected void onPostExecute(List<StationData> stationDatas) {
+        StationData train = stationDatas.get(0);
+        String notificationText = train.getDestination() + " - " + train.getExpDepart() + " (" + formatDelay(train.getLate()) + ")";
+        int notificationCode = Integer.valueOf(train.getTrainCode().substring(1, train.getTrainCode().length()));
 
+        Notification notification = new Notification.Builder(mContext)
+                .setSmallIcon(R.drawable.ic_stat_train)  // the status icon
+                .setContentTitle(notificationText)
+                .setTicker(notificationText)  // the status text
+                .setWhen(System.currentTimeMillis())  // the time stamp
+                .setContentText(notificationText)  // the contents of the entry
+                .setDeleteIntent(makeOnDeleteIntent(notificationCode))  // The intent to send when the entry is clicked
+        .build();
+
+        // Send the notification.
+        mNotificationManager.notify(notificationCode, notification);
+    }
+
+    private PendingIntent makeOnDeleteIntent(int notificationCode) {
+        Intent deleteIntent = new Intent(mContext, UpdateTrackingReceiver.class);
+        deleteIntent.putExtra("disabled", true);
+        return PendingIntent.getBroadcast(mContext, notificationCode, deleteIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+    }
+
+    private String formatDelay(int late) {
+        if(late >= 0) {
+            return "+" + late;
+        } else {
+            return String.valueOf(late);
         }
-//        textView.setText(result);
     }
 }
