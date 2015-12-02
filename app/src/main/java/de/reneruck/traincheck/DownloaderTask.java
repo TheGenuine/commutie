d55ae2653b9bd280b98a00206bfc9982b3c5d9a3
Rene Ruck <rene.ruck@eaglealpha.com>
2015-12-02 23:28:07 +0000

    reimplement onPostExecute in Downloader task and move the querying for the tracker in question into the task as well

--- app/src/main/java/de/reneruck/traincheck/DownloaderTask.java
+++ app/src/main/java/de/reneruck/traincheck/DownloaderTask.java
@@ -7,12 +7,15 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 
-import java.io.InputStream;
+import org.apache.commons.collections4.ListUtils;
+import org.apache.commons.collections4.Predicate;
+
 import java.util.List;
 
 import de.reneruck.traincheck.connection.ApiRequest;
 import de.reneruck.traincheck.connection.ApiResponseParser;
 import de.reneruck.traincheck.models.StationData;
+import de.reneruck.traincheck.models.Tracker;
 import de.reneruck.traincheck.receivers.UpdateTrackingReceiver;
 
 /**
@@ -20,13 +23,12 @@ import de.reneruck.traincheck.receivers.UpdateTrackingReceiver;
  * @author reneruck
  * @since 13/11/2015
  */
-public class DownloaderTask extends AsyncTask<String, Void, List<StationData>> {
-    private static final String DEBUG_TAG = "DownalodTask";
+public class DownloaderTask extends AsyncTask<Integer, Void, List<StationData>> {
+    private static final String DEBUG_TAG = "DownloadTask";
     private final NotificationManager mNotificationManager;
+    private Tracker mTracker;
 
     private Context mContext;
-
-    private String[] stations = {"LDWNE", "ASHTN", ""};
 
     public DownloaderTask(Context context) {
         mContext = context;
@@ -34,40 +36,58 @@ public class DownloaderTask extends AsyncTask<String, Void, List<StationData>> {
     }
 
     @Override
-    protected List<StationData> doInBackground(String... endpoints) {
-        String station = "ASHTN";
-        InputStream is = new ApiRequest("http://api.irishrail.ie/realtime/realtime.asmx/getStationDataByCodeXML?StationCode=" + station).execute();
-
-        List<StationData> stationDataList = ApiResponseParser.parseResult(is);
-        return pickTrackedOnes(stationDataList);
+    protected List<StationData> doInBackground(Integer ... trackerIds) {
+        mTracker = loadTracker(trackerIds[0]);
+        String station = mTracker.getPrimaryStation().replace(" ", "%20");
+        List<StationData> stationDataList = ApiResponseParser.parseResult(new ApiRequest("http://api.irishrail.ie/realtime/realtime.asmx/getStationDataByNameXML?StationDesc=" + station + "&NumMins=60").execute());
+        return selectMatchingTrains(stationDataList);
     }
 
-    private List<StationData> pickTrackedOnes(List<StationData> stationDataList) {
-        return stationDataList.subList(0, 1);
+    private Tracker loadTracker(int trackerId) {
+        return Tracker.find(mContext, trackerId);
+    }
+
+    private List<StationData> selectMatchingTrains(List<StationData> stationDataList) {
+        Predicate<StationData> selector = new Predicate<StationData>() {
+            @Override
+            public boolean evaluate(StationData entry) {
+                return entry.getDestination().equals(mTracker.getDirectionPrimary()) | entry.getDirection().equals(mTracker.getDirectionPrimary());
+            }
+        };
+        return ListUtils.select(stationDataList, selector);
     }
 
     @Override
-    protected void onPostExecute(List<StationData> stationDatas) {
-        StationData train = stationDatas.get(0);
-        String notificationText = train.getDestination() + " - " + train.getExpDepart() + " (" + formatDelay(train.getLate()) + ")";
-        int notificationCode = Integer.valueOf(train.getTrainCode().substring(1, train.getTrainCode().length()));
+    protected void onPostExecute(List<StationData> stationData) {
+        StationData trainOne = stationData.get(0);
 
-        Notification notification = new Notification.Builder(mContext)
-                .setSmallIcon(R.drawable.ic_stat_train)  // the status icon
-                .setContentTitle(notificationText)
-                .setTicker(notificationText)  // the status text
-                .setWhen(System.currentTimeMillis())  // the time stamp
-                .setContentText(notificationText)  // the contents of the entry
-                .setDeleteIntent(makeOnDeleteIntent(notificationCode))  // The intent to send when the entry is clicked
-        .build();
+        if(trainOne != null) {
+            String firstTrain = trainOne.getDestination() + " - " + trainOne.getSchDepart() + "(" + formatDelay(trainOne.getLate()) + ") In: " + trainOne.getDueIn() + "min";
+            String secondTrain = "";
+            if(stationData.size() > 1){
+                StationData trainTwo = stationData.get(1);
+                secondTrain = trainTwo.getDestination() + " - " + trainTwo.getSchDepart() + "(" + formatDelay(trainTwo.getLate()) + ") In: " + trainTwo.getDueIn() + "min";
+            }
+            int notificationCode = Integer.valueOf(trainOne.getTrainCode().substring(1, trainOne.getTrainCode().length()).trim());
 
-        // Send the notification.
-        mNotificationManager.notify(notificationCode, notification);
+            Notification notification = new Notification.Builder(mContext)
+                    .setSmallIcon(R.drawable.ic_stat_train)  // the status icon
+                    .setContentTitle(firstTrain)
+                    .setTicker(firstTrain)  // the status text
+                    .setWhen(System.currentTimeMillis())  // the time stamp
+                    .setContentText(secondTrain)  // the contents of the entry
+                    .setDeleteIntent(makeOnDeleteIntent(notificationCode, mTracker.getTrackerId()))  // The intent to send when the entry is clicked
+                    .build();
+
+            // Send the notification.
+            mNotificationManager.notify(notificationCode, notification);
+        }
     }
 
-    private PendingIntent makeOnDeleteIntent(int notificationCode) {
+    private PendingIntent makeOnDeleteIntent(int notificationCode, int trackerId) {
         Intent deleteIntent = new Intent(mContext, UpdateTrackingReceiver.class);
         deleteIntent.putExtra("disabled", true);
+        deleteIntent.putExtra("trackerId", trackerId);
         return PendingIntent.getBroadcast(mContext, notificationCode, deleteIntent, PendingIntent.FLAG_UPDATE_CURRENT);
     }
 
